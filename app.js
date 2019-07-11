9d5e1ccbefba287b6a912ed29872d18e36e184e4
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2019-07-11 06:31:57 +0100

    retry call

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+var counter = 1; 
+app.use(cors());
+
+app.get('/', function (req, res) {
+  var body = null;
+  counter > 5 ? body = 'COMPLETE' : body = 'PENDING';
+  res.send({status : body});
+
+  console.log('\x1b[34m%s\x1b[0m','Request number: ' + counter);
+  console.log('\x1b[34m%s\x1b[0m','Body: ' + body);
+  console.log('\x1b[32m%s\x1b[0m','=====================================')
+  body === 'COMPLETE' ? counter = 1 : counter++;
+});
+ 
+const port = 3000;
+app.listen(port,()=>console.log('Server is running in the port '+port))
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap, map, retryWhen, delay, timeout, catchError } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
+
+const timeoutVal = 130000;
+const delayTime = 1000;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'retry-client';
+  response: string;
+  tries = 1;
+
+  constructor(public httpClient: HttpClient) {}
+
+  request(): void {
+    this.response = 'START';
+    this.httpClient.get('http://localhost:3000').pipe(
+      tap((val: any) => {
+        if (val.status !== 'COMPLETE') {
+          throw val;
+        }
+        console.log(`OK!`);
+      }),
+      retryWhen(errors => errors.pipe(
+        tap(val => {
+          console.log('VALUE: ', val.status);
+          if (val.status === 'COMPLETE') {
+            throw val;
+          }
+          console.log(`Retrying...`);
+        }),
+        delay(delayTime),
+      )),
+      timeout(timeoutVal),
+      catchError(error => throwError(`Request timed out`)),
+    ).subscribe((value: any) => {
+      const res = value.status;
+      this.response = `COMPLETE with response: ${res}`;
+    }, error => console.log(error));
+  }
 }
